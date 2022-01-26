dfd21dbee38185315d5ecd497bfd471c7841f157
Fatima-M-Saadoon <91427755+Fatima-M-Saadoon@users.noreply.github.com>
2022-01-26 20:22:09 +0300

    Update admin.py

--- commerce/admin.py
+++ commerce/admin.py
@@ -4,7 +4,7 @@ from django.contrib.admin import TabularInline, ModelAdmin
 from commerce.models import (
     Product,
     Category,
-    Item, ProductImage
+    Item, ProductImage,Order
 )
 
 
@@ -19,23 +19,27 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['category']
     search_fields = ['name', 'qty', 'description', 'cost', 'price', 'discounted_price']
 
-#
-# class CategoryAdmin(admin.ModelAdmin):
-#     search_fields = ['category']
-#     list_display = ['id', 'name', 'description' ,'created','image']
-#
-#
-# class ItemAdmin(admin.ModelAdmin):
-#     list_display = ['id','user', 'product', 'item_qty', 'ordered',]
-#
-# class ProductImageAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'image']
 
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['category']
+    list_display = ['id', 'name', 'description' ,'created','image']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id','user', 'product', 'item_qty', 'ordered',]
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product','image']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user','ordered','total']
+
+
+
+admin.site.register(Order,OrderAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(Item)
-admin.site.register(ProductImage)
-#
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Item,ItemAdmin)
-# admin.site.register(ProductImage,ProductImageAdmin)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item,ItemAdmin)
+admin.site.register(ProductImage,ProductImageAdmin)
